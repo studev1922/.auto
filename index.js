a2e9fde51a3f93537c95f3131058bdffdc8e5c90
cakcak <sdhoa1922@gmail.com>
2025-05-20 00:06:40 +0700

    Refactor environment variables and update .gitignore; remove unused Facebook upload URLs and clean up package.json scripts

--- index.js
+++ index.js
@@ -4,14 +4,16 @@ import server from './src/server.js';
 import fbmodel from './src/fb_control.js';
 import m, { menu } from './src/module.js';
 
+
 const __dirname = path.dirname(fileURLToPath(import.meta.url))
 const mnu = {
     onFacebook: async () => await fbmodel(__dirname),
     onServer: async () => await server(__dirname),
 }
+await mnu.onFacebook();
 
-await menu.internalization([
-    ['Exit', null],
-    ['Facebook', mnu.onFacebook],
-    ['Manage File Upload', mnu.onServer],
-], false)
+// await menu.internalization([
+//     ['Exit', null],
+//     ['Facebook', mnu.onFacebook],
+//     ['Manage File Upload', mnu.onServer],
+// ], false)
